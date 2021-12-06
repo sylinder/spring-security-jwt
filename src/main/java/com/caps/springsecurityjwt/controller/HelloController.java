4b56adda177669b8f13544e634a2274d40b4e1e4
caps <yzsylinder@163.com>
2021-12-06 23:29:30 +0800

    init project

---
+++ src/main/java/com/caps/springsecurityjwt/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.caps.springsecurityjwt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Caps";
+    }
+}
